eda9bbcdbcbfed5fd1411112e888e776969872cb
federico <federicoberto708@gmail.com>
2025-12-10 16:53:52 +0700

    ref : chane dashboard feautre name to indonesian

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -42,7 +42,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
               </svg>
-              <span class="ml-3">Product</span>
+              <span class="ml-3">Produk</span>
             </div>
           </a>
         </li>
@@ -62,7 +62,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
               </svg>
-              <span class="ml-3">Product Stock</span>
+              <span class="ml-3">Stok Produk</span>
             </div>
           </a>
         </li>
@@ -82,7 +82,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25A2.25 2.25 0 0 1 13.5 8.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
               </svg>
-              <span class="ml-3">Item Categories</span>
+              <span class="ml-3">Kategori Barang</span>
             </div>
           </a>
         </li>
